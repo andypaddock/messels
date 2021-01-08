70fa7d009546071107b6f67c5d819d4087af554c
Andy Paddock <appaddock@gmail.com>
2021-01-08 17:13:59 +0000

    the actual days end

--- template-parts/accordian.php
+++ template-parts/accordian.php
@@ -1,12 +1,13 @@
 <?php if( have_rows('accordian') ): 
     $counter = 0; ?>
-    <div class="text-content accordion-outer mt5">
+<div class="text-content accordion-outer mt5">
     <?php while( have_rows('accordian') ): the_row();
     $counter++; ?>
-        <button class="info-accordion"><span class="auto-counter"><?php echo sprintf("%'.02d\n",$counter);?>.</span> <span><?php the_sub_field('accordian_header');?></span></button>
-<div class="info-panel">
-<?php the_sub_field('accordian_text');?>
-</div>
+    <button class="info-accordion"><span class="auto-counter">Q<?php echo sprintf("%'.01d\n",$counter);?>.</span>
+        <span><?php the_sub_field('accordian_header');?></span></button>
+    <div class="info-panel">
+        <?php the_sub_field('accordian_text');?>
+    </div>
     <?php endwhile; ?>
 </div>
 <?php endif; ?>
