ee6abd7e70ff8fec4211501f07d2735ccaf643ce
Andy Paddock <appaddock@gmail.com>
2021-06-21 16:45:27 +0100

    archives

--- page-templates/our-research.php
+++ page-templates/our-research.php
@@ -19,11 +19,46 @@ get_header();?>
                 <?php wp_list_categories( array(
         'orderby'    => 'name',
         'show_count' => true,
-        'exclude'    => array( 10 )
-    ) ); ?>
+        'exclude'    => array( 10 ),
+        'title_li' => '<h3 class="archive">' . __('Filter', 'textdomain') . '</h3>'
+     ) ); ?>
+            </ul>
+            <h3 class="archive">Arhive</h3>
+
+
+            <ul>
+                <?php
+/**/
+$years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date)
+FROM $wpdb->posts WHERE post_status = 'publish'
+AND post_type = 'post' ORDER BY post_date DESC");
+foreach($years as $year) :
+?>
+                <li class="archive-year"><?php echo $year; ?>
+
+                    <ul class="archive-sub-menu">
+                        <?    $months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date)
+        FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post'
+        AND YEAR(post_date) = '".$year."' ORDER BY post_date ASC");
+        foreach($months as $month) :
+        ?>
+                        <li><a href="<?php echo get_month_link($year, $month); ?>">
+
+                                <?php echo date( 'F', mktime(0, 0, 0, $month) );?></a>
+
+                        </li>
+
+                        <?php endforeach;?>
+
+                    </ul>
+
+                </li>
+
+                <?php endforeach; ?>
+
             </ul>
 
-            <?php wp_custom_archive(); ?>
+
         </div>
         <div class="research-archive__main"><?php 
 // the query
