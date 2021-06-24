38195b2a35c54d973fb4b821c36f003cb1eb5c48
Andy Paddock <appaddock@gmail.com>
2021-06-24 16:57:47 +0100

    excerpt

--- functions.php
+++ functions.php
@@ -256,8 +256,3 @@ function wp_custom_archive($args = '') {
     else
         return $output;
 }
-
-function custom_excerpt_length( $length ) {
-    return 20;
-}
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
